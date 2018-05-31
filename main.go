57769339d11426f00ce0d5022eadbbc0e43d7aea
Tanopwan Tangprakit <tanopwan@gmail.com>
2018-05-31 15:17:47 +0700

    Revise to middleware

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -11,39 +12,60 @@ import (
 func main() {
 
 	router := gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
+	router.GET("/hello", withRequestContext(), withTimeout(), controller)
+	router.Run(":8001")
+}
+
+// RequestIDKey ... context key
+type RequestIDKey struct{}
+
+var requestIDKey = RequestIDKey{}
+
+func withRequestContext() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		reqID := c.GetHeader("X-Request-ID")
+		ctx := context.WithValue(c, requestIDKey, reqID)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func withTimeout() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		var (
 			ctx    context.Context
 			cancel context.CancelFunc
 		)
 		timeout, err := time.ParseDuration("1s")
 		if err == nil {
-			ctx, cancel = context.WithTimeout(c, timeout)
+			ctx, cancel = context.WithTimeout(c.Request.Context(), timeout)
 		} else {
-			ctx, cancel = context.WithCancel(c)
+			ctx, cancel = context.WithCancel(c.Request.Context())
 		}
 		defer cancel()
 
 		c.Request = c.Request.WithContext(ctx)
 
-		doWork(ctx)
+		c.Next()
 
 		c.String(http.StatusOK, "Hello ธนพ\n")
-	})
-	router.Run(":8001")
+	}
 }
 
-func doWork(ctx context.Context) error {
+func controller(c *gin.Context) {
+	ctx := c.Request.Context()
 	done := make(chan bool)
 	go func() {
-		time.Sleep(5 * time.Second)
+		time.Sleep(3 * time.Second)
+		fmt.Printf("Value: %s\n", ctx.Value(requestIDKey))
 		done <- true
 	}()
 
 	select {
 	case <-ctx.Done():
-		return ctx.Err()
+		fmt.Println(ctx.Err())
+		return
 	case <-done:
-		return nil
+		return
 	}
 }
